c0caa9f70107bc776fe96e305152604e54d31c3f
dradnats1012 <herjebi1012@gmail.com>
2024-03-14 00:13:37 +0900

    240314 create repository

---
+++ busApi/src/main/java/koreatechBus/busApi/BusApiApplication.java
@@ -0,0 +1,13 @@
+package koreatechBus.busApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusApiApplication.class, args);
+	}
+
+}
